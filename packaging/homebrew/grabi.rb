86e66e558245e0bdaf7c5d5095d5280bd074fc0c
Ivan Rene Ramirez <ivanrene10@gmail.com>
2026-08-22 20:48:29 -0500

    Cask: 0.2.0

--- packaging/homebrew/grabi.rb
+++ packaging/homebrew/grabi.rb
@@ -7,8 +7,8 @@
 #
 # See packaging/homebrew/README.md for how and where to publish.
 cask "grabi" do
-  version "0.1.8"
-  sha256 "7ecef7c1487a45ca69bc6c0d4af32d515de92026cf855400201ce7c7d23e82b8" # printed by scripts/publish-release.sh
+  version "0.2.0"
+  sha256 "7765a15063157ced581702cc088671c668d555f1cb876863fd6433630918b73a" # printed by scripts/publish-release.sh
 
   url "https://dl.grabi.net/macos/v#{version}/Grabi-#{version}.dmg"
   name "Grabi"
